05451c13f2bdcc7a770cd03bed83eaa28cc9f59b
coder-muller <guilhermemullerxx@gmail.com>
2025-05-13 08:53:52 -0300

    [feat] update header component to include navigation link; wrap logo and label in a Link component for improved routing

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { Label } from "./ui/label";
 import { SidebarTrigger } from "./ui/sidebar";
 import Image from "next/image";
@@ -11,10 +12,12 @@ export default function Header() {
             <div className="flex items-center justify-center gap-2">
                 <SidebarTrigger />
             </div>
-            <div className="flex items-center justify-center gap-1">
-                <Label className="font-mono text-lg">Veltro</Label>
-                <Image src="/logo.png" alt="Veltro" width={32} height={32} />
-            </div>
+            <Link href="/">
+                <div className="flex items-center justify-center gap-1 cursor-pointer">
+                    <Label className="font-mono text-lg">Veltro</Label>
+                    <Image src="/logo.png" alt="Veltro" width={32} height={32} />
+                </div>
+            </Link>
         </header>
     )
 }
